4835dd01eaf56dbf06d222dca3879971d49fa7a2
KibreabTeshome <114220350+KibreabTeshome@users.noreply.github.com>
2023-03-31 16:04:22 +0300

    Create chrome.js

---
+++ chrome.js
@@ -0,0 +1,13 @@
+let myLeads=[];
+const inputEl=document.getElementById("input-btn");
+
+
+
+
+const inputBtn=document.getElementById("input-btn");
+
+inputBtn.addEventListener("click",function(){
+    myLeads.push("www.awesomelead.com")
+    console.log(myLeads)
+})
+
